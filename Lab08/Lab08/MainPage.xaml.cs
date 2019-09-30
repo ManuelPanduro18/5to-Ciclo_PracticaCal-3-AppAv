713486679edf6f017912885c3ef41c033eb05b46
ManuelPanduro18 <edson.panduro@tecsup.edu.pe>
2019-09-30 15:41:19 -0500

    Subiendo mi aplicación al Git

---
+++ Lab08/Lab08/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab08
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnStackL.Clicked += BtnStackL_Clicked;
+            btngrid.Clicked += Btngrid_Clicked;
+            btnHierchical.Clicked += BtnHierchical_Clicked;
+            btnTab.Clicked += BtnTab_Clicked;
+            btnForm.Clicked += BtnForm_Clicked;
+            btnDate.Clicked += BtnDate_Clicked;
+        }
+
+        private void BtnDate_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new DatePicker());
+        }
+
+        private void BtnForm_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new Form());
+        }
+
+        private void BtnTab_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new Tabbed());
+        }
+
+        private void BtnHierchical_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new Hierchical());
+        }
+
+        private void Btngrid_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new Grid());
+        }
+
+        private void BtnStackL_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new StackLayout());
+        }
+    }
+}
